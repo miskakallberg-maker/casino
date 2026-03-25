465b01a7914f4f0b4098540616275c29eeb1d67b
miskakallberg-maker <miskakallberg@gmail.com>
2026-03-25 19:40:18 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -25,6 +25,15 @@ const WITHDRAW_PENDING_FILE = path.join(BASE_DIR, 'withdraw_pending.txt');
 app.use(express.json());
 app.use(express.static(__dirname));
 
+// CORS for mobile access
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    if (req.method === 'OPTIONS') return res.sendStatus(200);
+    next();
+});
+
 let depositQueue = [];
 let onlineUsers = new Set();
 let chatMessages = [];
